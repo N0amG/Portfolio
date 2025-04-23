b75a4817f8435c16959145c549ca5bc35962d33a
N0amG <noamguez0@gmail.com>
2025-04-23 17:33:55 +0200

    Add : IconBox component

---
+++ src/components/IconBox.jsx
@@ -0,0 +1,13 @@
+import Icon from './Icon'
+
+// IconBox : icône, couleur icône, couleur titre, titre, className
+export default function IconBox({
+	iconPath,
+	bgColor = 'bg-slate-700',
+	iconColor = 'text-slate-900',
+	className = '',
+}) {
+	return (
+		<Icon path={iconPath} className={`w-[40px] h-[40px] ${iconColor} ${className} rounded-md`} />
+	)
+}
