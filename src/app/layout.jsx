6e4c46bf19a3a0cabebefcb7ab509c6a14bcd1eb
N0amG <noamguez0@gmail.com>
2025-04-30 12:43:58 +0200

    Add : Speed Metrics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import MainWrapper from '@/components/MainWrapper'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Définition des polices Google Fonts utilisées dans tout le site
 const geistSans = Geist({
@@ -49,6 +50,7 @@ export default function RootLayout({ children }) {
 			>
 					<MainWrapper>{children}</MainWrapper>
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	)
